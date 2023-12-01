6f01a281e476ea33ff74ccd369accce395c2fc9e
awiedeh <awiedeh@gmail.com>
2023-12-01 23:47:10 +0700

    modified:   app/dashboard/invoices/page.tsx 	modified:   app/ui/fonts.ts 	modified:   app/ui/search.tsx

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,5 @@
-import {Inter,Roboto,Agbalumo,Lusitana} from 'next/font/google';
+import {Inter,Roboto,Lusitana} from 'next/font/google';
 
 export const inter=Inter({subsets:['latin']});
 export const roboto=Roboto({weight:'400',subsets:['latin']});
-export const agbalumo=Agbalumo({weight:'400',subsets:['latin']});
 export const lusitana=Lusitana({subsets:['latin'],weight:['400','700']});
