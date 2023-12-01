0b2a98bc169e7e682ef8e38457018cd9f94a9989
awiedeh <awiedeh@gmail.com>
2023-12-01 14:16:20 +0700

    new file:   .env.example 	new file:   .eslintrc.json 	new file:   .gitignore 	new file:   .nvmrc 	new file:   README.md 	new file:   app/dashboard/customers/page.tsx 	new file:   app/dashboard/invoices/page.tsx 	new file:   app/dashboard/items/page.tsx 	new file:   app/dashboard/layout.tsx 	new file:   app/dashboard/page.tsx 	new file:   app/layout.tsx 	new file:   app/lib/data.ts 	new file:   app/lib/definitions.ts 	new file:   app/lib/placeholder-data.js 	new file:   app/lib/utils.ts 	new file:   app/page.tsx 	new file:   app/ui/acme-logo.tsx 	new file:   app/ui/button.tsx 	new file:   app/ui/customers/table.tsx 	new file:   app/ui/dashboard/cards.tsx 	new file:   app/ui/dashboard/latest-invoices.tsx 	new file:   app/ui/dashboard/nav-links.tsx 	new file:   app/ui/dashboard/revenue-chart.tsx 	new file:   app/ui/dashboard/sidenav.tsx 	new file:   app/ui/fonts.ts 	new file:   app/ui/global.css 	new file:   app/ui/home.module.css 	new file:   app/ui/invoices/breadcrumbs.tsx 	new file:   app/ui/invoices/buttons.tsx 	new file:   app/ui/invoices/create-form.tsx 	new file:   app/ui/invoices/edit-form.tsx 	new file:   app/ui/invoices/pagination.tsx 	new file:   app/ui/invoices/status.tsx 	new file:   app/ui/invoices/table.tsx 	new file:   app/ui/login-form.tsx 	new file:   app/ui/search.tsx 	new file:   app/ui/skeletons.tsx 	new file:   next.config.js 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.js 	new file:   prettier.config.js 	new file:   public/customers/amy-burns.png 	new file:   public/customers/balazs-orban.png 	new file:   public/customers/delba-de-oliveira.png 	new file:   public/customers/emil-kowalski.png 	new file:   public/customers/evil-rabbit.png 	new file:   public/customers/guillermo-rauch.png 	new file:   public/customers/hector-simpson.png 	new file:   public/customers/jared-palmer.png 	new file:   public/customers/lee-robinson.png 	new file:   public/customers/michael-novotny.png 	new file:   public/customers/steph-dietz.png 	new file:   public/customers/steven-tey.png 	new file:   public/favicon.ico 	new file:   public/hero-desktop.png 	new file:   public/hero-mobile.png 	new file:   public/opengraph-image.png 	new file:   scripts/seed.js 	new file:   tailwind.config.ts 	new file:   tsconfig.json

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import '@/app/ui/global.css';
+import {inter,roboto,agbalumo} from '@/app/ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import {Inter,Roboto,Agbalumo,Lusitana} from 'next/font/google';
+
+export const inter=Inter({subsets:['latin']});
+export const roboto=Roboto({weight:'400',subsets:['latin']});
+export const agbalumo=Agbalumo({weight:'400',subsets:['latin']});
+export const lusitana=Lusitana({subsets:['latin'],weight:['400','700']});
